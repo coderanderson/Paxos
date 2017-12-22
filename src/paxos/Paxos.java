bde1b8f8bafb08c5adf4780af508062931cb1388
coderanderson <mingzongzz@gmail.com>
2017-12-21 22:40:05 -0600

    Remove the original comment and add description

--- src/paxos/Paxos.java
+++ src/paxos/Paxos.java
@@ -1,3 +1,7 @@
+/*This file is part of the project which implement Paxos in java with java RMI. 
+*I choose this code because it has moderate complexity and shows my coding style.
+*/
+
 package paxos;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.server.UnicastRemoteObject;
@@ -6,9 +10,6 @@ import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.locks.ReentrantLock;
 import java.util.*;
 
-/**
- * This class is the main class you need to implement paxos instances.
- */
 public class Paxos implements PaxosRMI, Runnable{
 
     ReentrantLock mutex;
@@ -36,12 +37,8 @@ public class Paxos implements PaxosRMI, Runnable{
     int highestPrepare;
     int highestAccept;
     Instance highestAcceptV;
-
-    /**
-     * Call the constructor to create a Paxos peer.
-     * The hostnames of all the Paxos peers (including this one)
-     * are in peers[]. The ports are in ports[].
-     */
+    
+    
     public Paxos(int me, String[] peers, int[] ports){
 
         this.me = me;
@@ -70,20 +67,6 @@ public class Paxos implements PaxosRMI, Runnable{
         }
     }
 
-
-    /**
-     * Call() sends an RMI to the RMI handler on server with
-     * arguments rmi name, request message, and server id. It
-     * waits for the reply and return a response message if
-     * the server responded, and return null if Call() was not
-     * be able to contact the server.
-     *
-     * You should assume that Call() will time out and return
-     * null after a while if it doesn't get a reply from the server.
-     *
-     * Please use Call() to send all RMIs and please don't change
-     * this function.
-     */
     public Response Call(String rmi, Request req, int id){
         Response callReply = null;
 
@@ -106,24 +89,6 @@ public class Paxos implements PaxosRMI, Runnable{
         return callReply;
     }
 
-
-    /**
-     * The application wants Paxos to start agreement on instance seq,
-     * with proposed value v. Start() should start a new thread to run
-     * Paxos on instance seq. Multiple instances can be run concurrently.
-     *
-     * Hint: You may start a thread using the runnable interface of
-     * Paxos object. One Paxos object may have multiple instances, each
-     * instance corresponds to one proposed value/command. Java does not
-     * support passing arguments to a thread, so you may reset seq and v
-     * in Paxos object before starting a new thread. There is one issue
-     * that variable may change before the new thread actually reads it.
-     * Test won't fail in this case.
-     *
-     * Start() just starts a new thread to initialize the agreement.
-     * The application will call Status() to find out if/when agreement
-     * is reached.
-     */
     public void Start(int seq, Object value){
         // Your code here
         currSeq = seq;
@@ -295,24 +260,12 @@ public class Paxos implements PaxosRMI, Runnable{
         return new Response(true, 0, null);
     }
 
-    /**
-     * The application on this machine is done with
-     * all instances <= seq.
-     *
-     * see the comments for Min() for more explanation.
-     */
     public void Done(int seq) {
         // Your code here
         if(seq > doneSeq[me])
             doneSeq[me] = seq;
     }
 
-
-    /**
-     * The application wants to know the
-     * highest instance sequence known to
-     * this peer.
-     */
     public int Max(){
         // Your code here
         int max = 0;
@@ -323,34 +276,6 @@ public class Paxos implements PaxosRMI, Runnable{
         return max;
     }
 
-    /**
-     * Min() should return one more than the minimum among z_i,
-     * where z_i is the highest number ever passed
-     * to Done() on peer i. A peers z_i is -1 if it has
-     * never called Done().
-     *
-     * Paxos is required to have forgotten all information
-     * about any instances it knows that are < Min().
-     * The point is to free up memory in long-running
-     * Paxos-based servers.
-     *
-     * Paxos peers need to exchange their highest Done()
-     * arguments in order to implement Min(). These
-     * exchanges can be piggybacked on ordinary Paxos
-     * agreement protocol messages, so it is OK if one
-     * peers Min does not reflect another Peers Done()
-     * until after the next instance is agreed to.
-     *
-     * The fact that Min() is defined as a minimum over
-     * all Paxos peers means that Min() cannot increase until
-     * all peers have been heard from. So if a peer is dead
-     * or unreachable, other peers Min()s will not increase
-     * even if all reachable peers call Done. The reason for
-     * this is that when the unreachable peer comes back to
-     * life, it will need to catch up on instances that it
-     * missed -- the other peers therefore cannot forget these
-     * instances.
-     */
     public int Min(){
         // Your code here
         int min = doneSeq[me];
@@ -370,15 +295,6 @@ public class Paxos implements PaxosRMI, Runnable{
         return min + 1;
     }
 
-
-
-    /**
-     * the application wants to know whether this
-     * peer thinks an instance has been decided,
-     * and if so what the agreed value is. Status()
-     * should just inspect the local peer state;
-     * it should not contact other Paxos peers.
-     */
     public retStatus Status(int seq){
         // Your code here
         if(seq < Min()) return new retStatus(State.Forgotten, null);
@@ -404,7 +320,6 @@ public class Paxos implements PaxosRMI, Runnable{
     /**
      * Tell the peer to shut itself down.
      * For testing.
-     * Please don't change these four functions.
      */
     public void Kill(){
         this.dead.getAndSet(true);
